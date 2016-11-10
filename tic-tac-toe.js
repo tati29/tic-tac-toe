9efb475e9e1b2f38e4e0d9d5333487bd3a89ddd3
tatiana <tatimayala29@gmail.com>
2016-11-10 15:01:30 -0800

    set up game board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,13 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+
+  function topLeft(){
+  var myCircle = document.createElementNS(namespace, "circle")
+  myCircle.setAttribute("cx", 45)
+  myCircle.setAttribute("cy", 45)
+  myCircle.setAttribute("r", 30)
+  myCircle.setAttribute("fill", "black")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(myCircle)
+}
